f56c51f577f67a3110778852af9e6b8f5f56e773
MichaelOkoro24 <michaelokoro24@gmail.com>
2023-11-05 23:25:54 -0500

    directional light/loader added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         src={logo}
         alt="logo"
         className="w-13 h-11 object-contain rounded-full" />
-        <p className="text-white text-[18px] font-bold cursor-pointer"><span className="sm:block hidden">Michael Okoro</span>
+        <p className="text-white text-[18px] font-bold cursor-pointer"><span className="sm:block hidden">Michael Okoro | Portfolio</span>
 
         </p>
       </Link>
@@ -44,12 +44,13 @@ const Navbar = () => {
             //  changes the url after clicking the Links on the Navbar
              onClick={() => setActive(Link.title)}
           >
+           {/* This creates a href link to the array called navLinks in the constants/index.js path which connects to about, work, and contact on on the navbar*/}
             <a href={`#${Link.id}`}>{Link.title}
             </a>
           </li>
         ))} 
       </ul>
-      {/* This div creates a hamburger menu icon when the screen gets smaller */}
+      {/* This div creates a hamburger menu icon when the screen gets smaller which allows us to open and close the menue on smaller screens*/}
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
         src={toggle ? close : menu}
@@ -58,6 +59,32 @@ const Navbar = () => {
         object-contain cursor-pointer"
         onClick={() => setToggle(!toggle)}
         />
+        {/* Adds a black gradient menu you can toggle throuh when the hamburger menu is clicked on */}
+        <div className={`${toggle ? 'hidden': 'flex' } [p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+            {/* .map iterates through an array of navLinks stored in the index.js file*/}
+            {navLinks.map((Link) => (
+              <li
+                key={Link.id}
+                //  Checks if link is currently "active" by turning it grey when not active and white when active or hovering the text
+                className={`${
+                  active === Link.title
+                    ?"text-white"
+                    :"text-secondary"
+                }  font-poppins font-medium cursor-pointer text-[16px]`}
+                //  onClick arrow function that  changes the url after clicking the Links on the Navbar
+                onClick={() => {
+                  setToggle(!toggle); 
+                  setActive(Link.title);
+                }}
+              >
+              {/* This creates a href link to the array called navLinks in the constants/index.js path which connects to about, work, and contact on on the navbar*/}
+                <a href={`#${Link.id}`}>{Link.title}
+                </a>
+              </li>
+            ))} 
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+ import { Suspense, useEffect, useState } from "react";
+ // This imports an empty canvas allowing us to place something on it.
+ import { Canvas } from "@react-three/fiber";
+ // These imports will help us draw on the canvas and import 3-D models via useGLTF
+ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+ import CanvasLoader from "../Loader";
+
 
 const Computers = () => {
+   const computer = useGLTF("./desktop_pc/scene.gltf")
+
   return (
-    <div>Computers</div>
+    // When using three.js we'll use a mesh tag instead of a div tag.
+    <mesh>
+      {/* We need to add lights so we can see the computer model */}
+      <hemisphereLight intensity={0.15}
+      groundColor="black"/>
+       {/* Adds a glare to the monitor */}
+       <pointLight intensity={12}/>
+       <directionalLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, 1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      // The camera lets us know where were looking at the model from.
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader/>}>
+         {/* Allows us to move the model left and right. */}
+        <OrbitControls
+          enableZoom={false}
+          // This only allows us to turn the monitor horizontally.
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI /2}
+      />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas;
